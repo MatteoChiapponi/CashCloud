7c51f05a60f8d33550a0bc4785778ee808fa3055
Matteo Chiapponi <mateo.chiapponi@gmail.com>
2024-02-17 13:38:28 -0300

    renombrando ms de notificaciones

--- notifications-ms/src/main/java/com/mateo/notificationsms/NotificationsMsApplication.java
+++ notifications-ms/src/main/java/com/mateo/notificationsms/NotificationsMsApplication.java
@@ -1,13 +1,13 @@
-package com.mateo.notificationms;
+package com.mateo.notificationsms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationMsApplication {
+public class NotificationsMsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationMsApplication.class, args);
+		SpringApplication.run(NotificationsMsApplication.class, args);
 	}
 
 }
--- notifications-ms/src/test/java/com/mateo/notificationsms/NotificationsMsApplicationTests.java
+++ notifications-ms/src/test/java/com/mateo/notificationsms/NotificationsMsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.mateo.notificationms;
+package com.mateo.notificationsms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotificationMsApplicationTests {
+class NotificationsMsApplicationTests {
 
 	@Test
 	void contextLoads() {
