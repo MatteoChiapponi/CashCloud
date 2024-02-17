ee20ceddc4da766ed3758fa99d877d2b80981140
Matteo Chiapponi <mateo.chiapponi@gmail.com>
2024-02-17 12:43:00 -0300

    subierndo estructura del microservicio de notificaciones

---
+++ gateway/src/main/java/com/mateo/gateway/configuration/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.mateo.gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange(
+                authorizeExchangeSpec ->
+                                authorizeExchangeSpec.pathMatchers("/api/users/auth/**").permitAll()
+                                        .pathMatchers("/actuator/**").permitAll()
+                                        .anyExchange().authenticated()
+
+                        )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri("http://localhost:8080/realms/CashCloud_realm/protocol/openid-connect/certs")
+                        )
+                );
+        return http.build();
+    }
+}
